45b934015283fa2f318ab2a115c91481ff89efd8
datakun <data@kimjunu.com>
2016-10-05 17:06:14 +0900

    Update FTPCrawler.py

--- FTPCrawler.py
+++ FTPCrawler.py
@@ -47,6 +47,6 @@ if __name__ == "__main__":
     
     index = 0
     for url in ftp_url_list:
-        os.makedirs('~/videos/' + url + '/' + target_time + '/')
-        save_from_ftp(url, int(ftp_port_list[index]), username_list[index], password_list[index], '/tmp/hd1/record/' + target_time + '/', '~/videos/' + url + '/' + target_time + '/')
+        os.makedirs(os.expanduser('~') + '/videos/' + url + '/' + target_time + '/')
+        save_from_ftp(url, int(ftp_port_list[index]), username_list[index], password_list[index], '/tmp/hd1/record/' + target_time + '/', os.expanduser('~') + '/videos/' + url + '/' + target_time + '/')
         index += 1
